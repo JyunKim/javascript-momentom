4c42603fbac77318a311f4b2733be6d140e89369
JyunKim <kann1997@naver.com>
2021-01-29 20:15:33 +0900

    Debug clock

--- clock.js
+++ clock.js
@@ -6,9 +6,9 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.textContent = `${hours < 10 ? `0{hours}` : hours}:${
-    minutes < 10 ? `0{minutes}` : minutes
-  }:${seconds < 10 ? `0{seconds}` : seconds}`;
+  clockTitle.textContent = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
