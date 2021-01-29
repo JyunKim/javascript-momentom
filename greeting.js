965b85b072292b01276cd4dbfc2d417edf769a66
JyunKim <kann1997@naver.com>
2021-01-29 23:34:51 +0900

    Modify class name

--- greeting.js
+++ greeting.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('.js-form');
+const form = document.querySelector('.js-greeting-form');
 const input = form.querySelector('input');
 const greeting = document.querySelector('.js-greeting');
 
